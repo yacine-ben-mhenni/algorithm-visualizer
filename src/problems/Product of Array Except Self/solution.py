16d89432c51aea0797a764bc5ad33e158f1b4fe1
Yacine Ben Mhenni <yassoyacine0009@gmail.com>
2024-08-29 23:34:53 -0700

    Product of Array Except Self

---
+++ src/problems/Product of Array Except Self/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        answer = [1] * n
+        
+        # First pass: Calculate left products
+        left_product = 1
+        for i in range(n):
+            answer[i] = left_product
+            left_product *= nums[i]
+        
+        # Second pass: Calculate right products and combine with left products
+        right_product = 1
+        for i in range(n-1, -1, -1):
+            answer[i] *= right_product
+            right_product *= nums[i]
+        
+        return answer
